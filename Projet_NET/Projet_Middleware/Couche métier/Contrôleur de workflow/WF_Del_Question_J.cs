bf0dec42a32b0c3e3aa1c7b2618ad4ba51c14e6a
BlitzBanana <crepinvcc@gmail.com>
2014-05-14 11:13:51 +0200

    Ajout des WF de vincent
    Update et delete Questions O/J

---
+++ Projet_NET/Projet_Middleware/Couche métier/Contrôleur de workflow/WF_Del_Question_J.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageSerializable;
+using Projet_Middleware.Service_étendu;
+using Projet_Middleware.Couche_métier.Mappage;
+
+namespace Projet_Middleware.Couche_métier.Contrôleur_de_workflow
+{
+    class WF_Del_Question_J
+    {
+        public Message Exec(Message msg)
+        {
+            if (msg.Data[0].GetType() != typeof(int))
+                return msg;
+
+            msg.Data[0] = Mpg_Questions_Jeu.Rq_DeleteQuestion((int)msg.Data[0]);
+
+            msg = CAD.GetInstance().Execute_StockedProcedure(msg);
+
+            return msg;
+        }
+    }
+}
---
+++ Projet_NET/Projet_Middleware/Couche métier/Contrôleur de workflow/WF_Upd_Question_J.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageSerializable;
+using Projet_Middleware.Service_étendu;
+using Projet_Middleware.Couche_métier.Mappage;
+
+
+namespace Projet_Middleware.Couche_métier.Contrôleur_de_workflow
+{
+    class WF_Upd_Question_J
+    {
+        public Message Exec(Message msg)
+        {
+            if (msg.Data[0].GetType() != typeof(int) && msg.Data[1].GetType() != typeof(string) && msg.Data[2].GetType() != typeof(int))
+                return msg;
+
+            msg.Data[0] = Mpg_Questions_Jeu.Rq_UpdateQuestion((int)msg.Data[0], (string)msg.Data[1], (int)msg.Data[2]);
+
+            msg = CAD.GetInstance().Execute_StockedProcedure(msg);
+
+            return msg;
+        }
+    }
+}
