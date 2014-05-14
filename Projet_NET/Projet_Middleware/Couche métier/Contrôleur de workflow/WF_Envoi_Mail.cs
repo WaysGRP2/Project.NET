a6053c27198efaa4679128003956ffe7580ed2cc
BlitzBanana <crepinvcc@gmail.com>
2014-05-14 08:44:12 +0200

    Correction de l'envoi de mail

--- Projet_NET/Projet_Middleware/Couche métier/Contrôleur de workflow/WF_Envoi_Mail.cs
+++ Projet_NET/Projet_Middleware/Couche métier/Contrôleur de workflow/WF_Envoi_Mail.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Projet_Middleware.Couche_métier.Composant_technique;
 using MessageSerializable;
+using Projet_Middleware.Service_étendu;
 
 namespace Projet_Middleware.Couche_métier.Contrôleur_de_workflow
 {
     class WF_Envoi_Mail : IWorkflow
     {
-        SmtpMail mail;
-
-        public WF_Envoi_Mail()
-        {
-            mail = new SmtpMail();
-        }
-
         public Message Exec(Message msg)
         {
-            mail.send(msg);
+            if(msg.Data[0].GetType() == typeof(string) && msg.Data[1].GetType() == typeof(string))
+            {
+                ComSMTP.GetInstance().SendMail(msg);
+                return msg;
+            }
+
+            msg.Statut = false;
             return msg;
         }
     }
--- Projet_NET/Projet_Middleware/Service étendu/ComSMTP.cs
+++ Projet_NET/Projet_Middleware/Service étendu/ComSMTP.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 using Projet_Middleware.Couche_métier.Composant_technique;
 
 namespace Projet_Middleware.Service_étendu
@@ -10,11 +12,22 @@ namespace Projet_Middleware.Service_étendu
     class ComSMTP
     {
         private static ComSMTP instance = null;
+        private MailMessage message;
+        private SmtpClient smtp;
+        private NetworkCredential basicCredential;
         private XMLLoader xml;
+        private SMTPConfig config;
 
         private ComSMTP()
         {
             xml = new XMLLoader(XMLLoader.CONFIG_TYPE.SMTP_Config);
+            config = (SMTPConfig)this.GetSmtpConfig().Data[0];
+            message = new MailMessage();
+            smtp = new SmtpClient();
+            basicCredential = new NetworkCredential(config.Username, config.Password);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = basicCredential;
+
         }
 
         public static ComSMTP GetInstance()
@@ -30,6 +43,44 @@ namespace Projet_Middleware.Service_étendu
 
             MessageSerializable.Message msg = new MessageSerializable.Message();
             msg.Data[0] = config;
+            return msg;
+        }
+
+        public MessageSerializable.Message SendMail(MessageSerializable.Message msg)
+        {
+            if (msg.Data[0].GetType() == typeof(string))
+            {
+                try
+                {
+                    message.To.Add((string)msg.Data[0]);
+                    message.Subject = "JPO Exia Cesi";
+                    message.From = new System.Net.Mail.MailAddress(config.Address, "Exia Cesi");
+                    if ((string)msg.Data[1] == null)
+                    {
+                        msg.Statut = false;
+                        return msg;
+                    }
+                    else
+                    {
+                        message.Body = (string)msg.Data[1];
+                    }
+
+                    smtp.Host = config.Host;
+                    smtp.Port = config.Port;
+                    smtp.EnableSsl = config.EnableSSL;
+                    smtp.Send(message);
+
+                    msg.Statut = true;
+                    return msg;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERREUR :" + ex.Message);
+                    msg.Statut = false;
+                    return msg;
+                }
+            }
+            msg.Statut = false;
             return msg;
         }
     }
